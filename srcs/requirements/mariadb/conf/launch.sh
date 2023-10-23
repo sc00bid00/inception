8c09a027fc32c9b1f0b6d72a624735870b548775
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-10-23 20:15:11 +0200

    Commit wip 	modified:   srcs/requirements/mariadb/Dockerfile 	modified:   srcs/requirements/mariadb/conf/50-server.cnf 	modified:   srcs/requirements/mariadb/conf/launch.sh 	modified:   srcs/requirements/mariadb/conf/mariadb.cnf 	modified:   srcs/requirements/wordpress/conf/autoconfig.sh

--- srcs/requirements/mariadb/conf/launch.sh
+++ srcs/requirements/mariadb/conf/launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 service mariadb start;
 
+mariadb -uroot & MYSQL=$!
 while ! mysqladmin ping --silent; do
 	sleep 1
 done
@@ -8,14 +9,13 @@ done
 echo "Create database"
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 echo "Create user"
-mariadb -e "CREATE USER IF NOT EXISTS $MYSQL_USER@'localhost' IDENTIFIED BY $MYSQL_PASSWORD;"
-echo "Grant rpivileges"
-mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' IDENTIFIED BY $MYSQL_PASSWORD;"
+mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 echo "Grant privileges"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY $MYSQL_ROOT_PASSWORD;"
-echo "Flush"
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+echo "Alter flush"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
 echo "Shutdown"
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+wait $MYSQL
 echo "Start-up again"
 exec mysqld_safe
--- srcs/requirements/wordpress/conf/autoconfig.sh
+++ srcs/requirements/wordpress/conf/autoconfig.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-sleep 60
+
+# while ! mariadb ping --silent; do
+# 	echo "pinging"
+# 	sleep 1
+# done
+echo "Wordpress sleeps a minute"
+sleep 10
+echo "wordpress wakes up"
 wordpress_dir=/var/www/wordpress
 if [ ! -f "$wordpress_dir/wp-config.php" ]; then
 	wp config create	--allow-root \
