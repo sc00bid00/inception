1a3bb0d2d75b6b4a561f12eb225bf4a779715ff7
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-10-24 18:12:17 +0200

    wip changing with francescos help

--- srcs/requirements/wordpress/conf/autoconfig.sh
+++ srcs/requirements/wordpress/conf/autoconfig.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-sleep 1
-wordpress_dir=/var/www/wordpress
-if [ ! -f "$wordpress_dir/wp-config.php" ]; then
-	wp config create	--allow-root \
-						--dbname=$MYSQL_DATABASE \
-						--dbuser=$MYSQL_USER \
-						--dbpass=$MYSQL_PASSWORD \
-						--dbhost=mariadb:3306 \
-						--path='/var/www/wordpress'
-fi
-/usr/sbin/php-fpm7.3 -F
+
+cd /var/www/wordpress
+
+wp core config	--dbhost=mariadb \
+				--dbname=wordpress \
+				--dbuser=normal \
+				--dbpass=normal \
+				--allow-root
+
+wp core install --title=Inception \
+				--admin_user=wp \
+				--admin_password=wp \
+				--admin_email=fake_mail@mail.com \
+				--url=lsordo.42.fr \
+				--allow-root
+
+wp user create author fake_mail@gmail.com --role=author --user_pass=pass --allow-root
+
+cd -
+
+php-fpm7.4 -F
