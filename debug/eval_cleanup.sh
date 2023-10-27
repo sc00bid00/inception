2029895d9461c96b123ddfbdaacf297a294adf98
lsordo@vb <lsordo@student.42heilbronn.de>
2023-10-27 12:51:25 +0200

    Commit add eval_cleanup.sh command 	new file:   eval_cleanup.sh

---
+++ debug/eval_cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker stop $(docker ps -qa); docker rm $(docker ps -qa);
+docker rmi -f $(docker images -qa); docker volume rm $(docker volume ls -q); docker network rm $(docker network ls -q) 2>/dev/null
